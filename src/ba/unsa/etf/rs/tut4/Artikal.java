a1f2b1c85e87eda8ee1d265a213aec371e5358f0
fcolakovic2 <fcolakovic2@etf.unsa.ba>
2020-03-26 12:41:36 +0100

    Podešeni geteri, seteri, toString i konstruktor.

--- src/ba/unsa/etf/rs/tut4/Artikal.java
+++ src/ba/unsa/etf/rs/tut4/Artikal.java
@@ -1,4 +1,44 @@
 package ba.unsa.etf.rs.tut4;
 
 public class Artikal {
+
+    private String sifra;
+    private String naziv;
+    private double cijena;
+
+    public Artikal(String artikal) {
+        String[] info= artikal.split(",");
+        sifra = info [0];
+        naziv = info [1];
+        cijena= info [2];
+    }
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
+
+    @Override
+    public String toString() {
+        return "sifra" +"," +"naziv"+ ","+"cijena";
+    }
 }
