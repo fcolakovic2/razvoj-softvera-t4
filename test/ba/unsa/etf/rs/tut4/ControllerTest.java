aad1fa97fed6b4a3d48edc37cb58cd5088f7bec5
fcolakovic2 <fcolakovic2@etf.unsa.ba>
2020-04-03 18:23:04 +0200

    Napisana prva dva testa.

--- test/ba/unsa/etf/rs/tut4/ControllerTest.java
+++ test/ba/unsa/etf/rs/tut4/ControllerTest.java
@@ -5,7 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
@@ -24,6 +28,17 @@ class ControllerTest {
         stage.toFront();
     }
 
+    @Test
+    void Test1(FxRobot robot){
+        TextArea textArea2 = robot.lookup("#unos").queryAs(TextArea.class);
+        assertNotNull(textArea2);
+    }
 
+    @Test
+    void Test2(FxRobot robot){
+        robot.clickOn("#Račun");
+        Spinner spiner = robot.lookup("#kol").queryAs(Spinner.class);
+        assertNotNull(spiner);
+    }
 
 }
